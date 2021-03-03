f7239cb6bcf1cbaee4763e44517c4a95aacbc7a1
xeropise <whrbql1@gmail.com>
2021-03-03 22:49:16 +0900

    forth commit

---
+++ chapter_03/asyncOrderPromise.js
@@ -0,0 +1,20 @@
+const fs = require("fs").promisess;
+
+console.log("시작");
+
+fs.readFile("./readMe2.txt")
+  .then((data) => {
+    console.log("1번", data.toString());
+    return fs.readFile("./readMe2.txt");
+  })
+  .then((data) => {
+    console.log("2번", data.toString());
+    return fs.readFile("./readMe2.txt");
+  })
+  .then((data) => {
+    console.log("3번", data.toString());
+    console.log("끝");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
---
+++ chapter_03/sync.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+console.log("시작");
+
+let data = fs.readFileSync("./readMe2.txt");
+console.log("1번", data.toString());
+data = fs.readFileSync("./readMe2.txt");
+console.log("2번", data.toString());
+data = fs.readFileSync("./readMe2.txt");
+console.log("3번", data.toString());
+console.log("끝");
