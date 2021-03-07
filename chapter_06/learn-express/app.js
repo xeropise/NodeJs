7c9c2a2e57aa676daf737d5c45f41b304130a9d2
xeropise <whrbql1@gmail.com>
2021-03-07 18:11:12 +0900

    11th commit(2)

--- chapter_06/learn-express/app.js
+++ chapter_06/learn-express/app.js
@@ -27,6 +27,40 @@ app.use(
   })
 );
 
+// const multer = require("multer");
+// const fs = require("fs");
+
+// try {
+//   fs.readdirSync("uploads");
+// } catch (error) {
+//   console.error("uploads 폴더가 없어 uploads 폴더를 생성한다.");
+//   fs.mkdirSync("uploads");
+// }
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination(req, file, done) {
+//       done(null, "uploads/");
+//     },
+//     filename(req, file, done) {
+//       const ext = path.extname(file.originalname);
+//       done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+//     },
+//   }),
+//   limits: { fileSize: 5 * 1024 * 1024 },
+// });
+
+// app.get("/upload", (req, res) => {
+//   res.sendFile(path.join(__dirname, "multipart.html"));
+// });
+// app.post(
+//   "/upload",
+//   upload.fields([{ name: "image1" }, { name: "image2" }]),
+//   (req, res) => {
+//     console.log(req.files, req.body);
+//     res.send("ok");
+//   }
+// );
+
 app.use((req, res, next) => {
   console.log("모든 요청에 다 실행된다.");
   next();
