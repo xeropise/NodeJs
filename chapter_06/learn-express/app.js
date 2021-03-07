046e1bf2370170991e206390410538745d9f10fa
xeropise <whrbql1@gmail.com>
2021-03-07 23:03:40 +0900

    12th commit

--- chapter_06/learn-express/app.js
+++ chapter_06/learn-express/app.js
@@ -6,19 +6,27 @@ const dotenv = require("dotenv");
 const path = require("path");
 
 dotenv.config();
+const indexRouter = require("./routes");
+const userRouter = require("./routes/user");
+
 const app = express();
 app.set("port", process.env.PORT || 3000);
+// 템플릿 파일들이 위치한 폴더를 지정, res.render 메서드가 이 폴더 기준으로 템플릿을 찾아 렌더링
+app.set("views", path.join(__dirname, "views"));
+// 어떠한 종류의 템플릿 엔진을 사용할지를 나타낸다.
+app.set("view engine", "pug");
 
 app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+
 app.use(
   session({
     resave: false,
     saveUninitialized: false,
-    sercret: process.env.COOKIE_SECRET,
+    secret: process.env.COOKIE_SECRET,
     cookie: {
       httpOnly: true,
       secure: false,
@@ -27,55 +35,12 @@ app.use(
   })
 );
 
-// const multer = require("multer");
-// const fs = require("fs");
-
-// try {
-//   fs.readdirSync("uploads");
-// } catch (error) {
-//   console.error("uploads 폴더가 없어 uploads 폴더를 생성한다.");
-//   fs.mkdirSync("uploads");
-// }
-// const upload = multer({
-//   storage: multer.diskStorage({
-//     destination(req, file, done) {
-//       done(null, "uploads/");
-//     },
-//     filename(req, file, done) {
-//       const ext = path.extname(file.originalname);
-//       done(null, path.basename(file.originalname, ext) + Date.now() + ext);
-//     },
-//   }),
-//   limits: { fileSize: 5 * 1024 * 1024 },
-// });
-
-// app.get("/upload", (req, res) => {
-//   res.sendFile(path.join(__dirname, "multipart.html"));
-// });
-// app.post(
-//   "/upload",
-//   upload.fields([{ name: "image1" }, { name: "image2" }]),
-//   (req, res) => {
-//     console.log(req.files, req.body);
-//     res.send("ok");
-//   }
-// );
+app.use("/", indexRouter);
+app.use("/user", userRouter);
 
 app.use((req, res, next) => {
-  console.log("모든 요청에 다 실행된다.");
-  next();
+  res.status(404).send("Not Found");
 });
-
-app.get(
-  "/",
-  (req, res, next) => {
-    console.log("GET / 요청에서만 실행된다.");
-    next();
-  },
-  (req, res) => {
-    throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
-  }
-);
 
 app.use((err, req, res, next) => {
   console.error(err);
